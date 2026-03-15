4d95e2d95afc24a65799f78af138432f37e1adb0
Aufries <austfries@gmail.com>
2026-03-14 20:39:26 -0500

    Updated project structure

--- src/ui/ui_envhub.h
+++ src/ui/ui_envhub.h
@@ -6,8 +6,7 @@
 #define UI_ENVHUB_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /*********************
@@ -16,40 +15,41 @@ extern "C"
 #include "ui_envhub_gen.h"
 #include "ui_status_types.h"
 
-    /*********************
-     *      DEFINES
-     *********************/
+/*********************
+ *      DEFINES
+ *********************/
 
-    /**********************
-     *      TYPEDEFS
-     **********************/
+/**********************
+ *      TYPEDEFS
+ **********************/
 
-    /**********************
-     * GLOBAL VARIABLES
-     **********************/
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
 
-    /**********************
-     * GLOBAL PROTOTYPES
-     **********************/
-    void ui_envhub_init(void);
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void ui_envhub_init(void);
 
-    void ui_envhub_set_status_summary(status_severity_t severity, const char *text);
-    void ui_envhub_show_shutdown_popup(void);
+void ui_envhub_set_status_summary(status_severity_t severity, const char *text);
+void ui_envhub_show_shutdown_popup(void);
 
-    void ui_envhub_set_time_text(const char *s);
-    void ui_envhub_set_scd30(float co2_ppm, float temp_c, float humidity_rh);
-    void ui_envhub_set_bmp580(float pressure_hpa, float temperature_c);
-    void ui_envhub_set_sgp30(float eco2_ppm, float tvoc_ppb);
-    void ui_envhub_set_bq27441(uint8_t capacity_percent, float voltage_v, float current_ma);
+void ui_envhub_set_time_text(const char *s);
+void ui_envhub_set_scd30(float co2_ppm, float temp_c, float humidity_rh);
+void ui_envhub_set_bmp580(float pressure_hpa, float temperature_c);
+void ui_envhub_set_sgp30(float eco2_ppm, float tvoc_ppb);
+void ui_envhub_set_bq27441(uint8_t capacity_percent,float voltage_v,float current_ma);
 
-    /**
-     * Initialize the component library
-     */
-    void ui_envhub_init(void);
 
-    /**********************
-     *      MACROS
-     **********************/
+/**
+ * Initialize the component library
+ */
+void ui_envhub_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
 
 #ifdef __cplusplus
 } /*extern "C"*/
